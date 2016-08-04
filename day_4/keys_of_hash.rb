8cf459d5a5efbf44a0ee8556ee3765d6a49b5c6b
codeamac05 <codeamac05@gmail.com>
2016-08-04 13:53:44 -0500

    Refactored secret_identities to a method with some test cases

--- day_4/keys_of_hash.rb
+++ day_4/keys_of_hash.rb
@@ -3,13 +3,13 @@ Defines a method that receives a hash of your choice and return only the keys
 of the hash. For this exercise uses a variant of each method. Create a test method.
 =end
 
-secret_identities = {
-		'The Batman' => 'Bruce Wayne',
-		'Superman' => 'Clark Kent',
-		'Wonder Woman' => 'Diana Prince',
-		'Freakazoid' => 'Dexter Douglas'
-}
-
-secret_identities.each do |cover, name|
-	puts "#{cover}: #{name}"
+def secret_identities(super_heros)
+	super_heros.each do |cover, name|
+		return "#{cover}: #{name}"
+	end
 end
+
+p secret_identities('The Batman' => 'Bruce Wayne') == 'The Batman: Bruce Wayne'
+p secret_identities('Superman' => 'Clark Kent') == 'Superman: Clark Kent'
+p secret_identities('Wonder Woman' => 'Diana Prince') == 'Wonder Woman: Diana Prince'
+p secret_identities('Freakazoid' => 'Dexter Douglas') == 'Freakazoid: Dexter Douglas'
