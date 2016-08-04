c077eddd216cc468357bddd0e8f23036b5c13791
codeamac05 <codeamac05@gmail.com>
2016-08-03 20:20:14 -0500

    Refactored the method shopping

--- day_5/refactoring_shopping.rb
+++ day_5/refactoring_shopping.rb
@@ -1,10 +1,8 @@
 def shopping(list)
 	basket_shopping = Array.new
 
-	p list
-
-	#basket_shopping.push(list) if basket_shopping == basket_shopping.empty?
+	list.join(', ') if basket_shopping.empty?
 end
 
 #test
-p shopping(["eggs", "milk", "bread", "orange juice"]) #== "eggs, milk, bread, orange juice"
+p shopping(['eggs', 'milk', 'bread', 'orange juice']) == 'eggs, milk, bread, orange juice'
