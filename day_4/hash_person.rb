281584949558e3ae946f8a7b05ae83d4af9f6539
codeamac05 <codeamac05@gmail.com>
2016-08-04 21:38:08 -0500

    Hashing around

---
+++ day_4/hash_person.rb
@@ -0,0 +1,10 @@
+# Get the name of the person.
+# Add in the hash an occupation for the person.
+
+person = {first_name: 'Bob', email: 'bob@mail.com', phone: '12345690'}
+
+person[:job] = 'developer'
+
+p person[:first_name]
+
+p person
