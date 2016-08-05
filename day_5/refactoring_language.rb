984db32ebcdf9a3c37178f8484d2dccf556cbd8a
codeamac05 <codeamac05@gmail.com>
2016-08-05 10:50:15 -0500

    Refactored the language method to use then in the case

--- day_5/refactoring_language.rb
+++ day_5/refactoring_language.rb
@@ -1,25 +1,15 @@
 def language(speak)
 	case speak
-		when 'JavaScript'
-			'I like JavaScript'
-		when 'CSS'
-			'I like CSS'
-		when 'Python'
-			'I like Python'
-		when 'R'
-			'I like R'
-		when 'C++'
-			'I like C++'
-		when 'Unity'
-			'I like Unity'
-		when 'Ruby'
-			'I like Ruby'
-		when 'Ruby on Rails'
-			'I like Ruby on Rails'
-		when 'Java'
-			'I like Java'
-		else
-			'I don\'t like to code'
+		when 'JavaScript' then 'I like JavaScript'
+		when 'CSS' then 'I like CSS'
+		when 'Python' then 'I like Python'
+		when 'R' then 'I like R'
+		when 'C++' then 'I like C++'
+		when 'Unity' then 'I like Unity'
+		when 'Ruby' then 'I like Ruby'
+		when 'Ruby on Rails' then 'I like Ruby on Rails'
+		when 'Java' then 'I like Java'
+		else 'I don\'t like to code'
 	end
 end
 
