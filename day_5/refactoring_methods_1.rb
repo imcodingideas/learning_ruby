a1f6a96801ececb57e871c6e8b16c47ba9e7a248
codeamac05 <codeamac05@gmail.com>
2016-08-05 12:16:13 -0500

    Refactored to be one method and to one line

--- day_5/refactoring_methods_1.rb
+++ day_5/refactoring_methods_1.rb
@@ -3,20 +3,8 @@ Refactor this and the end should result should be a single method
 with a line of code. Make it pass the test.
 =end
 
-def a
-	true
-end
-
-def b
-	false
-end
-
-def join
-	a || b == b && b
-end
-
 def check?
-	!!join ? 'true' : 'false'
+	!!true || false == b && b ? 'true' : 'false'
 end
 
 #test
