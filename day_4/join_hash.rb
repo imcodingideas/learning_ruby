86a02706326f66a143df625e33fc2404de850ee6
codeamac05 <codeamac05@gmail.com>
2016-08-05 11:47:07 -0500

    Completed the join_hash method

--- day_4/join_hash.rb
+++ day_4/join_hash.rb
@@ -3,16 +3,22 @@ Defines a method join_hash that receives three hashes and returns the
 union of the hashes. Cannot use merge.
 =end
 
-def join_hash(fruit)
-	joined_hash = Hash.new(0)
-	joined_hash.push({fruit})
+def join_hash(*args)
+	joined = Hash.new
+
+	args.each do |element|
+		element.each do |k, v|
+			joined[k] = v
+		end
+	end
+
+	joined
 end
 
+#hashes
 fruit = {name: 'pineapple'}
 weight = {weight: '1 kg'}
 taste = {taste: 'good'}
 
-
 #test
-p join_hash(fruit) #== {:name=> 'pineapple', :weight=> '1 kg', :taste=> 'good'}
-# p join_hash(fruit, weight, taste) #== {:name=> 'pineapple', :weight=> '1 kg', :taste=> 'good'}
+p join_hash(fruit, weight, taste) == {:name=> 'pineapple', :weight=> '1 kg', :taste=> 'good'}
