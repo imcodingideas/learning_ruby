c5fb3ecb9d6737f2fc6d2b48d16a84cf8dedff1b
Joseph Chambers <joseph@michael-chambers.com>
2016-08-09 16:33:35 -0500

    Method to count the values in the array between the two numbers. Thanks @dylanerichards for the help simple version

--- day_6/total_between_age.rb
+++ day_6/total_between_age.rb
@@ -9,10 +9,22 @@ Maximum Age (max_age)
 And how many people the Array returns have aged MIN_AGE and max_age.
 =end
 
-def total_between_age(array_of_ages, min_age, max_age)
+# def total_between_age(ages, min_age, max_age)
+#   total = 0
+#
+#   ages.each do |age|
+#     if age >= min_age && age <= max_age
+#       total += 1
+#     end
+#   end
+#
+#   total
+# end
 
+def total_between_age(ages, min_age, max_age)
+  ages.select { |num| num >= min_age && num <= max_age }.count
 end
 
 # Tests
-p total_between_age([10, 20, 30, 40, 50, 60], 20, 40) #== 3
-p total_between_age([18, 19, 20, 23, 24, 27], 20, 27) #== 4
+p total_between_age([10, 20, 30, 40, 50, 60], 20, 40) == 3
+p total_between_age([18, 19, 20, 23, 24, 27], 20, 27) == 4
