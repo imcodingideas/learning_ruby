05c69ca502006b6eacb89af08159e126f438f028
Joseph Chambers <joseph@michael-chambers.com>
2016-08-10 10:23:55 -0500

    Learning about classes

---
+++ day_7/create_a_class.rb
@@ -0,0 +1,19 @@
+class Cat
+end
+
+class Bug
+end
+
+class Dog
+end
+
+class Dolphin
+end
+
+cat = Cat.new
+car = Bug.new
+window = Dolphin.new
+water = Dog.new
+fruit = Dolphin.new
+vegetable = Cat.new
+person = Dog.new
