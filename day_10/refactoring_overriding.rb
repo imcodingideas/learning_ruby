6d9fc77a401c59a8fa85c039597943bbe6a9573b
Joseph Chambers <joseph@michael-chambers.com>
2016-08-12 10:07:49 -0500

    Completed with a module

--- day_10/refactoring_overriding.rb
+++ day_10/refactoring_overriding.rb
@@ -1,42 +1,34 @@
-class SuperSay
+module SuperSay
   def say(text)
-    prepare_text(text)
-  end
-
-  def prepare_text(text)
-    do_something_with(text)
+    text
   end
 end
 
 class HtmlSay
-  # noinspection RubyDeadCode
-  def say(text)
-    "<p>" + prepare_text(text) + "</p>"
-  end
-
+  include SuperSay
   def say(text)
     prepare_text(text)
   end
 
   def prepare_text(text)
-
+    "<p>#{text}</p>"
   end
 end
 
 class CssSay
-  def say(text)
-    "<p>" + prepare_text(text) + "</p>"
-  end
+  include SuperSay
 
   def prepare_text(text)
     do_something_with(text)
   end
 
   def do_something_with(text)
-
+    text
   end
 end
 
+doc = HtmlSay.new
+style = CssSay.new
 
 #test
 p doc.say("You've refactored") == "<p>You've refactored</p>"
