2d73f1df687dcd861028e624eeffa9d72d1cc7a3
Joseph Chambers <joseph@michael-chambers.com>
2016-08-17 14:37:29 -0500

    Added benchmark

--- day_13/binary_search.rb
+++ day_13/binary_search.rb
@@ -1,17 +1,22 @@
+require 'benchmark'
+
 def find_index(values, target)
   binary_search(values, target, 0, values.length-1)
 end
 
 def binary_search(values, target, start, last)
   middle = (start + last)/2
+  value = values[middle]
 
-  if values[middle] > target
+  if value > target
     binary_search(values, target, start, middle)
-  elsif values[middle] < target
+  elsif value < target
     binary_search(values, target, middle+1, last)
   else
     middle
   end
 end
+
+p Benchmark.realtime { find_index([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 20) == 9 } * 1000
 
 p find_index([1, 4, 6, 7, 12, 13, 15, 18, 19, 20, 22, 24], 20) == 9
