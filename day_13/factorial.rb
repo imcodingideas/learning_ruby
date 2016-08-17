6d4b7956bbb3b18a81d3e192c79755d09f41dd7b
Joseph Chambers <joseph@michael-chambers.com>
2016-08-17 14:42:09 -0500

    Added some benchmarks

--- day_13/factorial.rb
+++ day_13/factorial.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 def factorial_iterative(n)
   sum = 1
   until n == 0
@@ -7,6 +9,7 @@ def factorial_iterative(n)
   sum
 end
 
+p Benchmark.realtime { factorial_iterative(5) == 120 } * 1000
 p factorial_iterative(5) == 120
 
 
@@ -17,4 +20,5 @@ def factorial_recursive(n)
   factorial[n]
 end
 
+p Benchmark.realtime { factorial_recursive(100) == 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000 } * 1000
 p factorial_recursive(5) == 120
