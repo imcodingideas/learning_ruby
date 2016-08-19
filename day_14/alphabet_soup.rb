bed44749f828eaa1fcd69cc2bddb62906ed89f99
Joseph Chambers <joseph@michael-chambers.com>
2016-08-19 11:02:29 -0500

    Still need to refactor

--- day_14/alphabet_soup.rb
+++ day_14/alphabet_soup.rb
@@ -1,26 +1,73 @@
 class Board
-# You should put here the given boards templates
-# @@boards_templates =
-  def initialize
-    complete_board!
+  # You should put here the given boards templates
+  def initialize(string)
+    @mix = string.gsub(/[X]/){|x| ("A".."Z").to_a.sample}
+    @board =  @mix.scan(/.{5}/).map!{|row| row.split("")}
   end
 
-  def to_s
-    "try to print a board, what does this method do?"
+  def to_s(board=nil)
+    board = @board if board.nil?
+    board.each do |row|
+      row.each do |element|
+        print "  #{element}  "
+      end
+      puts
+    end
+    " "
   end
 
-  private
-
-  def complete_board!
-    # This method should fill an incomplete board, in other words change the "x" by random letters.
+  def check_board(word)
+    new_board = []
+    @board.map{|row| new_board << row.dup}
+    diagonal = convert_diagonals_to_cols(new_board)
+    (check_rows(@board, word) || check_rows(@board.transpose, word) || check_rows(diagonal, word) || check_rows(diagonal.transpose, word)) ?
+        true : false
   end
+
+  def check_rows(board, word)
+    board.transpose.each do |row|
+      return true if word_in_array(row,word.upcase)
+      return true if word_in_array(row.join.reverse.split(""),word.upcase)
+    end
+    false
+  end
+
+  def convert_diagonals_to_cols(board_diagonal)
+    board_diagonal.each_with_index do |row, index|
+      index.times do
+        row.unshift(" ")
+      end
+      (board_diagonal.length-index).times do
+        row.push(" ")
+      end
+    end
+    board_diagonal
+  end
+
+  def word_in_array(array,word)
+    array.join.include?(word) ? true : false
+  end
+
 end
 
-board = Board.new
+# Tests
+words = ["POEMA", "CANCION", "RONDAS", "RIMAS", "SAMIR"]
+board = Board.new("POEMAXCXXXXAXXSXNXAAXCMXDXIXXNROXXOXNXXR")
 puts board
+words.each do |word|
+  p board.check_board(word)
+end
 
-# Boards words and Templates
+words1 = ["MANGO", "SANDIA", "MELON", "PLATANO"]
+board1 = Board.new("XXXXPXXXXLXAMXAXIEXTXDLXAXNOXNMANGOXSXXX")
+puts board1
+words1.each do |word|
+  p board1.check_board(word)
+end
 
-[["POEMA", "CANCION", "RONDAS", "RIMAS"],"POEMAXCXXXXAXXSXNXAAXCMXDXIXXNROXXOXNXXR"]
-[["MANGO", "SANDIA", "MELON", "PLATANO"],"XXXXPXXXXLXAMXAXIEXTXDLXAXNOXNMANGOXSXXX"]
-[["ROJO", "VERDE", "AZUL", "NEGRO"],"OJORXXXXXXXEXXOXDXRXXRGLXXEXUXNVXZXXXXAX"]
+words2 = ["ROJO", "VERDE", "AZUL", "NEGRO" ]
+board2 = Board.new("OJORXXXXXXXEXXOXDXRXXRGLXXEXUXNVXZXXXXAX")
+puts board2
+words2.each do |word|
+  p board2.check_board(word)
+end
