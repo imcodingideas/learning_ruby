cde63f270011623d194a46307dff0c870695da70
Joseph Chambers <joseph@michael-chambers.com>
2016-08-23 12:32:33 -0500

    Added test case, joined the array

--- day_12/pig_latin.rb
+++ day_12/pig_latin.rb
@@ -18,15 +18,18 @@ def pig_word
     if word.split(/[aeiouAEIOU].*/) == []
       pig_latin.push(word + 'way')
     else
-       n = 0
-       n += 1 while (word[n].count "aeiouAEIOU") != 1
+      n = 0
+      n += 1 while (word[n].count "aeiouAEIOU") != 1
       pig_latin.push(word[n..word.length-1] + word.split(/[aeiouAEIOU].*/).first + "ay")
     end
   end
 
-  pig_latin
+  pig_latin.join(' ')
 end
 
+# Tested with running:
+# ruby pig_latin.rb pig banana trash apple elephant egg duck
+p pig_word == "igpay ananabay ashtray appleway elephantway eggway uckday"
 p pig_word
 
 
@@ -48,7 +51,7 @@ RETURN the pig-latin sentence, and strip extra
 space at the end
 =end
 
-
+#
 # def pig_sentence(string)
 #   pig_latin = ""
 #   string.split().each do |word|
