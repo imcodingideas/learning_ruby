6cc74ac72b56e51aeabc176fbcb19dcaf04e0e2b
Joseph Chambers <joseph@michael-chambers.com>
2016-09-01 13:08:26 -0500

    Created the user model to pass the tests: rspec spec/user_spec.rb -e "#name and #age"

--- day_23/user_schema/app/models/user.rb
+++ day_23/user_schema/app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
 # Implementa los métodos y validaciones de tu modelo aquí. 
 
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :message => "Enter a valid Email address!"},
+            :uniqueness => {:case_sensitive => false, :message => "Email already exists!"}
+  validates :age, :numericality => { :greater_than => 18}
+  validates :phone, :length => {:minimum => 10}
+
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    age = Date.today.year - birthday.year
+    age -= 1 if Date.today < birthday + age.years
+    age
+  end
+
 end
