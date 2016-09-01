f5aa92444d5b56876aa54e18a0060a484618cd6f
Joseph Chambers <joseph@michael-chambers.com>
2016-09-01 12:48:05 -0500

    created the migration for the users table

--- day_23/user_schema/db/migrate/20121011144238_create_users.rb
+++ day_23/user_schema/db/migrate/20121011144238_create_users.rb
@@ -1,2 +1,15 @@
 # Este archivo debe contener una migración 
 
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :email
+      t.string :phone
+      t.date :birthday
+      t.timestamps
+    end
+  end
+end
